6474604c2058613e04fd5dd66d2e2a6e411c605d
Ajay U <ajayudayasanu95@gmail.com>
2021-11-02 22:58:48 +0530

    added semantic ui, added routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from 'semantic-ui-react'
+import DashBoard from "./routes/DashBoard/DashBoard";
+import LoginPage from "./routes/Login/login";
 function App() {
   return (
-    <div className="App">
-     <h1>Hello</h1>
-    </div>
+    <Router>
+      <div>Header</div>
+      <Container>
+        <Route path='login' component={LoginPage}/>
+        <Route path='/' component={DashBoard} exact />
+      </Container>
+    </Router>
   );
 }
 
